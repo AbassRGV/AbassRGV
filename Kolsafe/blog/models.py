0324845ff73b82a3649e038e9be7a670b8832377
Abass <syllaabass6@gmail.com>
2024-09-04 12:38:06 -0400

    revenir au premier commite

--- Kolsafe/blog/models.py
+++ Kolsafe/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.timezone import now
+
+
+class BlogDB(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    dateblog = models.DateField(null=True, blank=True)
+    source = models.CharField(max_length=100,)
+    img = models.ImageField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
